6d1beb6ae637b15107a3c118170a7851a6f74ef8
Yemmieeyc <adewole_aina@yahoo.com>
2024-08-26 21:48:45 -0700

    wip

--- api/server.js
+++ api/server.js
@@ -1,3 +1,26 @@
-// BUILD YOUR SERVER HERE
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const express = require('express')
+
+const User = require('./users/model')
+
+const server = express()
+
+server.get('/api/users', (req, res) =>{
+    User.find()
+    .then(users =>{
+        throw new Error('arghhh')
+    })
+    .catch(err => {
+        res.json(500).json({ 
+            message: "There was an error while saving the user to the ",
+        err: err.message })
+    })
+})
+
+server.use('*', (req, res) => {
+    res.status(404).json({
+        message: 'not found'
+    })
+})
+
+module.exports = server; 
